6f47a9158caf9df3f19c6520e88c7ee453e1aa47
AsaduzzamanSam <samrat.asad@gmail.com>
2021-10-04 12:29:49 +0600

    make a plain scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text('I Am Inevitable'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+    ),
+  ),);
 
 }
 
